d3ab6b235ffc04379b716723a2536dd13615099c
JorgeVZC8 <jorgevzc8@gmail.com>
2025-03-20 20:47:13 +0100

    Revision Api games solucionado

--- game-service-api/src/main/java/com/example/game_service_api/controller/impl/GameController.java
+++ game-service-api/src/main/java/com/example/game_service_api/controller/impl/GameController.java
@@ -21,7 +21,7 @@ public class GameController implements GameApi {
 
     @Override
     @PostMapping
-    public ResponseEntity<Game> saveGame(@RequestHeader("userIdRequest")  String userId, @RequestBody Game game){
+    public ResponseEntity<Game> saveGame(@RequestHeader("userIdRequest") String userId, @RequestBody Game game){
         System.out.println(userId);
         var gameCreated = this.gameService.saveGame(game);
         return ResponseEntity.ok(gameCreated);
