5bdd276ae3942a2ab7e72aab41bd5955ac0bd021
JorgeVZC8 <jorgevzc8@gmail.com>
2026-02-16 12:51:25 +0100

    Pruebas finales auth

--- game-service-api/src/main/java/com/example/game_service_api/controller/GameApi.java
+++ game-service-api/src/main/java/com/example/game_service_api/controller/GameApi.java
@@ -12,11 +12,11 @@ public interface GameApi {
     @PostMapping
     ResponseEntity<Game> saveGame(@RequestHeader String userId, @RequestBody Game game);
     @GetMapping("/{id}")
-    ResponseEntity<Game> getGameById(String id);
+    ResponseEntity<Game> getGameById(@PathVariable String id);
     @GetMapping
     ResponseEntity<List<Game>> getAllGames();
-    @DeleteMapping
-    ResponseEntity<String> deleteGame(Long id);
+    @DeleteMapping("/{id}")
+    ResponseEntity<String> deleteGame(@PathVariable Long id);
     @PutMapping("/{id}")
     ResponseEntity<Game> putGame(@PathVariable Long id,@RequestBody Game gameRequest);
 
